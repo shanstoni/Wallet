903bfe20971801580e1a996ade741be2288b0673
shanstoni <blazejportala@gmail.com>
2017-08-27 00:57:46 +0200

    Retrofit 2

---
+++ app/src/main/java/com/example/shan/wallet/data/model/remote/RetrofitClient.java
@@ -0,0 +1,25 @@
+package com.example.shan.wallet.data.model.remote;
+
+/**
+ * Created by shan on 26.08.2017.
+ */
+
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient(String baseUrl) {
+        if (retrofit==null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(baseUrl)
+                    .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
---
+++ app/src/main/java/com/example/shan/wallet/data/model/remote/SOService.java
@@ -0,0 +1,23 @@
+package com.example.shan.wallet.data.model.remote;
+
+/**
+ * Created by shan on 26.08.2017.
+ */
+
+import com.example.shan.wallet.data.model.SOAnswersResponse;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+import rx.Observable;
+
+public interface SOService {
+
+    @GET("user")
+    Observable<SOAnswersResponse> getAnswers();
+
+    //@GET("/answers?order=desc&sort=activity&site=stackoverflow")
+    //Call<SOAnswersResponse> getAnswers(@Query("tagged") String tags);
+}
